5c3c9640a25969a1af624b19d3f51134d5c3262b
Nip403 <francoisjiayan@gmail.com>
2025-01-06 00:19:36 +0000

    Day 17(1) & Day 18

--- Day 17/solution.py
+++ Day 17/solution.py
@@ -1,14 +1,77 @@
 from pathlib import Path
+from collections import deque
+import numpy as np
 
 with open(Path(__file__).parent / "input_data.txt", "r") as f:
     data = f.read()
+    
+def update(combo: np.ndarray, output: list[int], opcode: int, operand: int, pointer: int) -> int:
+    match opcode: # my very first use of match, incredible 
+        case 0: # floor divide A by 2 ** combo operand, equiv to bitwise rshift
+            combo[4] >>= combo[operand]
 
+        case 1: # xor B with literal operand
+            combo[5] ^= operand
+            
+        case 2: # combo modulo 8 -> B
+            combo[5] = combo[operand] % 8
+
+        case 3: # if A is not 0, set pointer to literal operand
+            if combo[4]:
+                pointer = operand - 2 # offset the pointer increase (not supposed to update after instruction)
+                
+        case 4: # B xor= C
+            combo[5] ^= combo[6]
+            
+        case 5: # output combo operand mod 8
+            output.append(combo[operand] % 8)
+            
+        case 6: # same as 0, store in B
+            combo[5] = combo[4] >> combo[operand]
+            
+        case 7: # same as 0, store in C
+            combo[6] = combo[4] >> combo[operand]
+            
+        case _:
+            raise
+        
+    return pointer
+
+def gen_output(registers: np.ndarray, program: np.ndarray) -> list[int]:
+    pointer = 0
+    combo = np.empty(7, dtype=int)
+    combo[:] = np.array([0, 1, 2, 3, *registers], dtype=int) # keep track of register values in here, A=4
+
+    output = deque()
+
+    while pointer < program.shape[0]:
+        opcode, operand = program[pointer: pointer + 2] # check if out of bounds error arises (pointer should always be at least 2 left from end)
+        pointer = update(combo, output, opcode, operand, pointer) + 2
+                    
+    return output
+
+def backtrack(program: np.ndarray) -> int:
+    output = program.tolist()
+    
+    
+    
 def main(data: str) -> tuple[int]:
-    # Part 1
+    registers, program = data.split("\n\n")
+    registers = np.array([int(i.split(": ")[1]) for i in registers.splitlines()])
+    program = np.array([int(i) for i in program.split(": ")[1].split(",")])
+    
+    print(program)
+    data2 = """Register A: 2024
+Register B: 0
+Register C: 0
 
-    # Part 2
+Program: 0,3,5,4,3,0"""
 
-    return
+    return ",".join(str(i) for i in gen_output(registers, program)), backtrack(program)
+
+
+
+
 
 if __name__ == "__main__":
-    print(main(data))
+    print(main(data)) 
--- Day 18/solution.py
+++ Day 18/solution.py
@@ -1,14 +1,120 @@
 from pathlib import Path
+import numpy as np
+from collections import deque
+from typing import NewType, Callable
+import heapq
+import copy
+import re
+
+Coordinate = NewType("Coordinate", tuple[int])
+
+UP = np.array([-1, 0], dtype=int)
+DOWN = np.array([1, 0], dtype=int)
+LEFT = np.array([0, -1], dtype=int)
+RIGHT = np.array([0, 1], dtype=int)
 
 with open(Path(__file__).parent / "input_data.txt", "r") as f:
     data = f.read()
 
+class MemorySpace:
+    def __init__(self, corrupt_order: np.ndarray, shape: np.ndarray = (71, 71)) -> None:
+        self.idx = deque(corrupt_order)
+        self.space = np.zeros(shape, dtype=int)
+        self.shape = shape
+    
+    def __iter__(self) -> None:
+        return self
+    
+    def __next__(self) -> None:
+        self.space[*self.idx.popleft()] = -1
+        
+    def __str__(self) -> str:
+        lookup = np.array([" ", "O", "."], dtype="str")
+        return "\n".join("".join(row) for row in lookup[self.space])
+        
+    def bounds(self, coord: Coordinate) -> bool:
+        return 0 <= coord[0] < self.shape[0] and 0 <= coord[1] < self.shape[1]
+
+    def astar(self, start: Coordinate, end: Coordinate) -> list[Coordinate]:
+        def reconstruct_path(came_from: dict[Coordinate, Coordinate], current: Coordinate) -> list[Coordinate]:
+            path = [current]
+            
+            while current in came_from.keys():
+                current = came_from[current]
+                path.append(current)
+            
+            return path[::-1]
+        
+        # manhattan heuristic over euclidean for only cardinal movements
+        heuristic: Callable[Coordinate, float] = lambda yx: np.sum(np.abs(np.array(yx) - end))
+        
+        open_set = [(heuristic(start), tuple(start))]
+        os_set = {tuple(start)}
+        came_from = {}
+        
+        g = np.full(self.shape, fill_value=np.inf, dtype=float)
+        f = np.full(self.shape, fill_value=np.inf, dtype=float)
+        
+        g[*start] = 0
+        f[*start] = heuristic(start)
+        
+        while open_set:
+            f0, current = heapq.heappop(open_set)
+            os_set.remove(current)
+        
+            if np.all(current == end):
+                return reconstruct_path(came_from, current)
+            
+            for direction in [UP, DOWN, LEFT, RIGHT]:
+                check = np.array(current) + direction
+                
+                if not self.bounds(check) or self.space[*check] == -1:
+                    continue
+                
+                if (tentative:= g[*current] + 1) < g[*check]:
+                    came_from[tuple(check)] = current
+                    g[*check] = tentative
+                    f[*check] = tentative + heuristic(check)
+                    
+                    if not tuple(check) in os_set:
+                        heapq.heappush(open_set, (f[*check], tuple(check)))
+                        os_set.add(tuple(check))
+        
+        return []
+    
+    def find_block(self, start: Coordinate, end: Coordinate) -> Coordinate:
+        path = self.astar(start, end)
+        
+        while True: 
+            candidate = self.idx[0]
+            next(self)
+            
+            if tuple(candidate) in path:
+                path = self.astar(start, end)
+                
+                if not path:
+                    return candidate[::-1]
+    
+    def print_solved(self, start: Coordinate, end: Coordinate) -> None:
+        results = tuple(zip(*self.astar(start, end)))
+        self.space[results] = 1
+        print(self)
+        self.space[results] = 0
+
 def main(data: str) -> tuple[int]:
-    # Part 1
+    fall = np.reshape(re.split(r"\n|,", data), (-1, 2)).astype(int)
+    start = np.zeros(2, dtype=int)
+    end = np.array([70, 70], dtype=int)   
+        
+    space = MemorySpace(fall[:, ::-1]) # [[y x]]
+    
+    for _ in range(1024):
+        next(space)
+     
+    snapshot = copy.deepcopy(space)
+    #space.print_solved(start, end)
 
-    # Part 2
-
-    return
+    return len(snapshot.astar(start, end)) - 1, ",".join(str(i) for i in space.find_block(start, end))
 
 if __name__ == "__main__":
     print(main(data))
