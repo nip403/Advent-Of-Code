7ad3afed8300d7b6511866820692030d7ab811e5
Nip403 <francoisjiayan@gmail.com>
2025-01-08 21:45:22 +0000

    Day 20 (2)

--- Day 20/solution.py
+++ Day 20/solution.py
@@ -55,7 +55,7 @@ class Track:
             current += move
             self.track.append(tuple(current))
 
-    def gen_cheats(self) -> dict[tuple[Coordinate], int]:
+    def gen_cheats_2(self) -> dict[tuple[Coordinate], int]:
         cheats = defaultdict(list)
         
         for wall, tracks in self.exposed.items():
@@ -69,30 +69,27 @@ class Track:
                     cheats[wall].append(i - idx[0] - 2) # dont ask me why, this is an off by 2 error somehow
             
         return Counter(i for saved_list in sorted(cheats.values()) for i in saved_list)
+    
+    def gen_cheats_20(self) -> dict[int, int]: # returns {seconds_saved: number_cheats}
+        cheats = defaultdict(list)
+        track = np.array(self.track)
+        manhattan_diff = np.sum(np.abs(track[:, None] - track), axis=2) # sum of track abs differences
+
+        for p1 in range(len(track)):
+            p2_idx = np.where((np.arange(len(track)) > p1) & (manhattan_diff[p1] <= 20))[0] # only consider valid cheats within 20ps and that bring you forward
+            saved = (p2_idx - p1) - manhattan_diff[p1, p2_idx]
+
+            for s in saved[saved > 1]:
+                cheats[tuple(track[p1])].append(s)
+                
+        return Counter(i for saved_list in sorted(cheats.values()) for i in saved_list)
 
 def main(data: str) -> tuple[int]:
     track = Track(data)
-    skips = track.gen_cheats()
-
-    return sum(v for k, v in skips.items() if k >= 100)
+    skips2 = track.gen_cheats_2()
+    skips20 = track.gen_cheats_20()
+        
+    return sum(v for k, v in skips2.items() if k >= 100), sum(v for k, v in skips20.items() if k >= 100)
     
 if __name__ == "__main__":
-    
-    data = """###############
-#...#...#.....#
-#.#.#.#.#.###.#
-#S#...#.#.#...#
-#######.#.#.###
-#######.#.#...#
-#######.#.###.#
-###..E#...#...#
-###.#######.###
-#...###...#...#
-#.#####.#.###.#
-#.#...#.#.#...#
-#.#.#.#.#.#.###
-#...#...#...###
-###############"""
-    
-    
     print(main(data))
